c7433763883c685ee343ab067364905e4419cfb7
Jordan Janzen <jordan@morels.me>
2025-11-22 22:51:38 -0800

    Revert module chunking

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,38 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const chunkGroups = [
-  {
-    name: "react-vendors",
-    test: (id: string) => /node_modules\/(react|react-dom|react-router-dom)\//.test(id),
-  },
-  { name: "dnd-kit", test: (id: string) => /node_modules\/@dnd-kit\//.test(id) },
-  { name: "radix-ui", test: (id: string) => /node_modules\/@radix-ui\//.test(id) },
-  { name: "tanstack", test: (id: string) => /node_modules\/@tanstack\//.test(id) },
-];
-
-const manualChunks = (id: string) => {
-  if (!id.includes("node_modules")) {
-    return undefined;
-  }
-  for (const group of chunkGroups) {
-    if (group.test(id)) {
-      return group.name;
-    }
-  }
-  return "vendor";
-};
-
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1200,
-    rollupOptions: {
-      output: {
-        manualChunks,
-      },
-    },
-  },
   server: {
     port: 5173,
     strictPort: true,
